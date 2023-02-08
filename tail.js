eef47f86a0a3c90fe12bc71c0ecc3ae883be3cef
Yevheniia Horbachova <gor.jenia2003@gmail.com>
2023-02-08 00:07:40 -0700

    Added function to return all other elements of array

---
+++ tail.js
@@ -0,0 +1,22 @@
+const isAgrumentsEqual = function (a, b) {
+    if (a == b) {
+        return true
+    }
+}
+
+const assertEqual = function (actual, expected) {
+    if (isAgrumentsEqual(actual, expected) === true ) {
+        console.log(`✅✅✅ Assertion Passed: ${actual} and ${expected} are equal.`);
+
+    } else {
+        console.log(`❌❌❌ Assertion Failed: ${actual} and ${expected} are NOT equal.`);
+    }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
+const tail = function(array) {
+    return array.slice(1);
+  };
