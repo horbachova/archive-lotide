ea5aad9e6c068a0ebe88f814e5aab317fba1b52e
Yevheniia Horbachova <gor.jenia2003@gmail.com>
2023-02-08 22:04:26 -0700

    Added function to find index of letter's position

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const isArraysEqual = function (arr1, arr2) {
+    if (arr1.length = arr2.length) {
+        for (let i = 0; i < arr2.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                return false
+            }
+        } return true
+    } return false
+}
+
+
+const assertArraysEqual = function (actual, expected) {
+    if (isArraysEqual(actual, expected) === true ) {
+        console.log(`✅✅✅ Assertion Passed: ${actual} and ${expected} are equal.`);
+
+    } else {
+        console.log(`❌❌❌ Assertion Failed: ${actual} and ${expected} are NOT equal.`);
+    }
+};
+
+const letterPositions = function(string) {
+    const results = {};
+  
+    for (const [index, letter] of string.split('').entries()) {
+      if (letter !== ' ') {
+        if (results[letter]) {
+          results[letter].push(index);
+        } else {
+          results[letter] = [index];
+        }
+      }
+    }
+  
+    return results;
+  };
+  
+
+  assertArraysEqual(letterPositions("hello").e, [1]);
+
