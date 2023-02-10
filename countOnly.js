113fb3ddee4ee5bd2e261d5e57ed7a6c97715603
Yevheniia Horbachova <gor.jenia2003@gmail.com>
2023-02-10 15:45:45 -0700

    Updated function to count true keys in object[2]

--- countOnly.js
+++ countOnly.js
@@ -1,32 +1,17 @@
-const isAgrumentsEqual = function(a, b) {
-  if (a === b) {
-    return true;
-  }
-};
-
-const assertEqual = function(actual, expected) {
-  if (isAgrumentsEqual(actual, expected) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} and ${expected} are equal.`);
-
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} and ${expected} are NOT equal.`);
-  }
-};
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
-  for (const item of allItems) {
-    if (itemsToCount[item]) {
-      if (results[item]) {
-        results[item] += 1;
-      } else {
-        results[item] = 1;
-      }
+  for (const key of allItems) {
+    if (!itemsToCount[key]) {
+      continue;
     }
+
+    if (results[key]) {
+      results[key] = 0;
+    }
+    //Happy Path
+    results[key]++;
   }
   return results;
 };
@@ -44,8 +29,3 @@ const firstNames = [
 ];
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
