04c47c9bbb07c23f208ee20fa659bc7b0bf918a7
Yevheniia Horbachova <gor.jenia2003@gmail.com>
2023-02-10 15:02:21 -0700

    Updated function to compare actual and expected

--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,10 @@
-const isAgrumentsEqual = function (a, b) {
-    if (a == b) {
-        return true
-    }
-}
-
-const assertEqual = function (actual, expected) {
-    if (isAgrumentsEqual(actual, expected) === true ) {
-        console.log(`✅✅✅ Assertion Passed: ${actual} and ${expected} are equal.`);
-
-    } else {
-        console.log(`❌❌❌ Assertion Failed: ${actual} and ${expected} are NOT equal.`);
-    }
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} and ${expected} are equal.`);
+    return;
+  }
+  //Happy Path
+  console.log(`❌❌❌ Assertion Failed: ${actual} and ${expected} are NOT equal.`);
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
