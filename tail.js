30beecdb6396d10cf9db82de47961592a0f6f62a
Yevheniia Horbachova <gor.jenia2003@gmail.com>
2023-02-10 19:47:24 -0700

    Updated tail function

--- tail.js
+++ tail.js
@@ -1,22 +1,3 @@
-const isAgrumentsEqual = function (a, b) {
-    if (a == b) {
-        return true
-    }
-}
-
-const assertEqual = function (actual, expected) {
-    if (isAgrumentsEqual(actual, expected) === true ) {
-        console.log(`✅✅✅ Assertion Passed: ${actual} and ${expected} are equal.`);
-
-    } else {
-        console.log(`❌❌❌ Assertion Failed: ${actual} and ${expected} are NOT equal.`);
-    }
-};
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-
 const tail = function(array) {
-    return array.slice(1);
-  };
+  return array.slice(1);
+};
