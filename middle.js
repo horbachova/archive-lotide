9459dc257b02a7161fb48082202f93d43d9e7f14
Yevheniia Horbachova <gor.jenia2003@gmail.com>
2023-02-12 02:32:39 -0700

    Added function to return the middle-most element(s) of the given array

---
+++ middle.js
@@ -0,0 +1,16 @@
+
+
+const middle = function(array) {
+  const midIndex = Math.floor(array.length / 2);
+  
+  if (array.length <= 2) {
+    return [];
+  
+  } else if (array.length % 2 === 0) {
+    return [array[midIndex - 1], array[midIndex]];
+  
+  } else {
+    return [array[midIndex]];
+  }
+  
+};
